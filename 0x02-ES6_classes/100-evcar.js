b4ca050ad17b26c3a1ca6f1b2df6caf0dd4eb7d0
ofperez-TheOwl <odidiperez@gmail.com>
2022-12-25 11:46:40 +0000

    Complete Project 0X02

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,9 +1,12 @@
-export default class Car {
-  constructor(brand, motor, color) {
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    // call constructor of super class (Building)
+    super(brand, motor, color);
+
     // Create objs
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this._range = range;
   }
 
   // Methods
